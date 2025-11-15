f05c88095771c339883a59a228ae615b96744668
John <johnalsoares@gmail.com>
2025-11-14 21:17:33 -0300

    Update generate person function

--- cypress/functions/generate-person.js
+++ cypress/functions/generate-person.js
@@ -14,11 +14,12 @@ export default function generatePerson() {
     .toString();
   const phone = faker.phone.number({ style: "international" });
   const clean = name
+    .replace(/\b\w+\./g, "")
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
-    .replace(/\s+/g, "")
+    .replace(/[^a-zA-Z]/g, "")
     .toLowerCase();
-  // O método da const clean é remover acentos e unir os nomes gerados pelo faker. Há casos, como o nome núbia, que gera o e-mail com acento e isso não funcionaria.
+  // O método da const clean é remover acentos, unir os nomes gerados pelo faker e remover prefixos. Há casos, como o nome núbia, que gera o e-mail com acento ou  gera nomes como Srta. Ana e isso não funcionaria na validação.
   const email = `${clean}@example-test.com`;
   // Como não há uma validação específica para provedores de e-mail, coloquei um e-mail padrão para diminuir o uso de recursos.
   const passwd = faker.internet.password();
